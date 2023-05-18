0df761894b8dd312f6a9a659ae14d56354fdaf9c
Max Hirtenstein <maxhirtens@gmail.com>
2023-05-18 13:56:12 -0700

    delete modules

--- backend/config.js
+++ backend/config.js
@@ -5,15 +5,17 @@
 require("dotenv").config();
 require("colors");
 
+const env = process.env;
+
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
 const PORT = +process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
-  return (process.env.NODE_ENV === "test")
-      ? "jobly_test"
-      : process.env.DATABASE_URL || "jobly";
+  return process.env.NODE_ENV === "test"
+    ? "jobly_test"
+    : process.env.DATABASE_URL || "jobly";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
@@ -26,6 +28,7 @@ console.log("SECRET_KEY:".yellow, SECRET_KEY);
 console.log("PORT:".yellow, PORT.toString());
 console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
 console.log("Database:".yellow, getDatabaseUri());
+console.log("env:".yellow, env);
 console.log("---");
 
 module.exports = {
