87d6c996966528fc0a695a560a1b0e6f41cadea8
Max Hirtenstein <maxhirtens@gmail.com>
2023-05-18 14:04:09 -0700

    fixing env

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://localhost:10000" || "http://localhost:3001";
+const BASE_URL = "http://localhost:10000" || "http://localhost:3001";
 
 /** API Class.
  *
